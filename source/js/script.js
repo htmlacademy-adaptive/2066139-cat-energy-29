76b74bea6c51253c462caa3335e26a2438fe779b
GitRemka <roman141999@mail.ru>
2023-08-22 17:51:42 +0700

    Добавляет адаптивную графику и фиксы

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,11 @@ let headerMain = document.querySelector('.main-header');
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 
+// На случай неработоспособности скрипта показываем меню в потоке документа
 headerMain.classList.remove('main-header--nojs');
+if (! headerMain.classList.contains('main-header--nojs')) {
+  navMain.classList.add('main-nav--closed');
+};
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
