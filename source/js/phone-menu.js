f35a7a41dfa8845eea26e68347e56491b0437083
GitRemka <roman141999@mail.ru>
2023-08-30 14:07:17 +0700

    Производит рефакторинг скрипта для мобильного меню

--- source/js/phone-menu.js
+++ source/js/phone-menu.js
@@ -9,11 +9,6 @@ if (!headerMain.classList.contains('main-header--nojs')) {
 };
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
+  navMain.classList.toggle('main-nav--closed');
+  navMain.classList.toggle('main-nav--opened');
 });
